7e031c3125219dd9d131d30ff0ebe530603bad56
nycwy <stuxy.1129@gmail.com>
2025-06-02 15:37:26 +0545

    pre increment and post increment

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
-// let score = null
+let score = null
 
 // console.log(typeof score)
 // console.log(typeof (score))
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber)
 
@@ -11,9 +11,9 @@
 // "33abc" => NaN
 // true => 1; false => 0
 
-// let isLoggedIn = 1
+let isLoggedIn = 1
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(typeof booleanIsLoggedIn)
 // console.log(booleanIsLoggedIn)
 
@@ -22,6 +22,33 @@
 // "santosh" => true
 
 let someNumber = 33
+
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// **************************************** (OPERATIONS) ****************************************
+
+let value = 3
+let negValue = - value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Santosh"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)  //12
+// console.log(1 + "2")  //12
+// console.log("1" + 2 + 2)  //122
+// console.log(1 + 2 + "2")  //32
+
+// console.log(true)
+// console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+// console.log(gameCounter)
+
+// https://tc39.es/ecma262/multipage/ecmascript-language-expressions.html#sec-postfix-increment-operator
