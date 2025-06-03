8ce792797033724aa7c993c7543aed7a5312f146
nycwy <stuxy.1129@gmail.com>
2025-06-03 16:38:28 +0545

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score)
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) //two value after decimal point
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString()) //by default American standard = 1,000,000
+// console.log(hundreds.toLocaleString('en-IN')) //Indian standard = 10,00,000
+
+
+
+
+// ==================== (Maths) ====================
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3, 4, 5, 6, 2))
+// console.log(Math.max(3, 4, 5, 6, 2))
+
+// console.log(Math.floor(Math.random() * 10) + 1)
+// Math.random() => value b/w 0 to 1
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min)) //to get the value within a range
