58a09f33d91a0c6a5828644c81e0222e1dddd2bb
nycwy <stuxy.1129@gmail.com>
2025-06-16 10:04:29 +0530

    constructor function (new keyword)

--- 10_classes_and_oop/OOP.js
+++ 10_classes_and_oop/OOP.js
@@ -18,3 +18,22 @@ const user = {
 // console.log(this); //global object
 
 // CONSTRUCTOR FUNCTION
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    // return this;
+}
+
+const user1 = new User("Santosh", 8, true);
+const user2 = new User("Angourie", 4, false);
+console.log(user2.username);
+
+// #*#* 'new' KEYWORD #*#*
+// - Creates a new empty object.
+// - Sets the prototype of that object to the constructor’s .prototype.
+// - Binds this inside the constructor to the new object.
+// - Executes the constructor function with the provided arguments.
+// - Returns the new object automatically (unless the constructor returns another object explicitly).
