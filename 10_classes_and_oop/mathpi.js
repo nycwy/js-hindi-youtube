aa19842aba55b41bf668b87d0ae38233ed2a4434
nycwy <stuxy.1129@gmail.com>
2025-06-17 18:40:12 +0530

    more on object (descriptor)

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,22 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+// console.log(descriptor);
+
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true
+};
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+Object.defineProperty(chai, "name", {
+    // writable: false,
+    enumerable: false
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} : ${value}`);
+    }
+}
